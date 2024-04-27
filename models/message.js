623211e3f717b3310dd6fbe99165cfd283bd9256
LarsdeVries04 <151883289+LarsdeVries04@users.noreply.github.com>
2024-04-27 12:52:21 +0200

    Add files via upload

---
+++ models/message.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  ID: {
+    type: String,
+  },
+  Fname: {
+    type: String,
+    required: true,
+  },
+  Lname: {
+    type: String,
+    required: true,
+  },
+  Email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true,
+    minLength: 8,
+  },
+  Password: {
+    type: String,
+    required: true,
+    
+  },
+  Postcode:{
+    type: String,
+    default: "--",
+  },
+  Address:{
+    type: String,
+    default: "--",
+  }
+  
+});
+module.exports = mongoose.model("Userdata", UserSchema);
